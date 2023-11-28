ce357f251ff83b9e5320152b78b440892223d049
Rosa Maggi <rosa.maggi@epfl.ch>
2023-11-28 17:27:35 +0100

    [Feature] Add text

---
+++ src/stories/molecules/inputFields/Text.stories.tsx
@@ -0,0 +1,101 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Text } from './Text';
+import React from "react";
+
+const meta = {
+  title: 'Molecules/Inputs/Text',
+  component: Text,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'text'
+  }
+};
+
+export const Required: Story = {
+  args: {
+    isRequired: true,
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'text'
+  }
+};
+
+export const Readonly: Story = {
+  args: {
+    isReadonly: true,
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'text'
+  }
+};
+
+export const WithLabel: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    label: 'Test label',
+    type: 'text'
+  }
+};
+
+export const WithHelp: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    help: 'Test label',
+    type: 'text'
+  }
+};
+
+export const WithPlaceholder: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    placeholder: 'Test label',
+    type: 'text'
+  }
+};
+
+export const Password: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'password'
+  }
+};
+
+export const Email: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'email'
+  }
+};
+
+export const Telephone: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'tel'
+  }
+};
+
+export const Url: Story = {
+  args: {
+    name: 'test_Name',
+    id: 'test_primary',
+    type: 'url'
+  }
+};
---
+++ src/stories/molecules/inputFields/Text.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import '../../assets/custumStyles.css';
+
+interface TextProps {
+  size?: 'small' | 'medium' | 'large';
+  isReadonly?: boolean;
+  value?: string;
+  placeholder?: string;
+  label?: string;
+  isRequired?: boolean;
+  id: string;
+  name: string;
+  help?: string;
+  type: 'text' | 'tel' | 'email' | 'password' | 'url';
+  minlength?: number;
+  maxlength?: number;
+  pattern?: string;
+  validationErrorMessage?: string;
+  onChange?: () => void;
+}
+
+export const Text = ({
+                          size = 'medium',
+                          isReadonly = false,
+                          value,
+                          placeholder,
+                          label,
+                          isRequired = false,
+                          id,
+                          name,
+                          help,
+                          validationErrorMessage,
+                          type,
+                          minlength,
+                          maxlength,
+                          pattern,
+                          onChange
+                        }: TextProps) => {
+
+  let formControlStyle: string = 'form-control';
+  switch (size) {
+    case 'small':
+      formControlStyle = formControlStyle.concat(' form-control-sm')
+      break;
+    case 'large':
+      formControlStyle = formControlStyle.concat(' form-control-lg')
+      break;
+  }
+
+  const requiredStyle = isRequired ? 'form-group required' : 'form-group';
+
+  return (
+    <div className={requiredStyle}>
+      {label ? <label className='control-label' htmlFor={id}>{label}</label> : <></>}
+      <div className="input-group has-validation">
+        <input
+          className={formControlStyle}
+          type={type}
+          id={id}
+          name={name}
+          minLength={minlength}
+          maxLength={maxlength}
+          pattern={pattern}
+          placeholder={placeholder}
+          value={value}
+          readOnly={isReadonly}
+          required={isRequired}
+        />
+        <div className="invalid-feedback">
+          {validationErrorMessage}
+        </div>
+      </div>
+      {help ? <small className="form-text text-muted">{help}</small> : <></>}
+    </div>
+  )
+};
