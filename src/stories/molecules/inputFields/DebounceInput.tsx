2e406be06ea205847e99ed597cc89bc0262cd7b8
Rosa Maggi <rosa.maggi@epfl.ch>
2024-01-18 14:47:36 +0100

    [Feature] Add debounce input

---
+++ src/stories/molecules/inputFields/DebounceInput.tsx
@@ -0,0 +1,58 @@
+import React, {useMemo, useRef, useState} from 'react';
+import { FormControlProps, Form } from 'react-bootstrap';
+import './autocomplete.css';
+import {debounce} from "p-debounce-throttle";
+
+interface DebounceInputProps {
+  input?: string;
+  placeholder?: string;
+  isReadonly?: boolean;
+  onChange?: (inputValue: string) => void;
+}
+
+/**
+ * An autocomplete list with debounce query.
+ *
+ * If single selection: the selected element is directly shown in the input field.
+ * The "itemValue", if present, is the initialization value.
+ *
+ * For multiple selections: selected elements are shown above the input list and it is possible to delete them.
+ * The "selected" prop contains all selected items.
+ */
+export const DebounceInput = ({
+   input = '',
+   placeholder = '',
+   isReadonly = false,
+   onChange
+  }: DebounceInputProps & FormControlProps) => {
+  const [inputValue, setInputValue] = useState(input);
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  const debouncedCallback = useMemo(() => {
+    const func = (value: string) => {
+        onChange?.(value);
+    };
+
+    return debounce(func, 1000);
+  }, [onChange]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setInputValue(value);
+    debouncedCallback(value);
+  };
+
+  return (
+    <div ref={inputRef}>
+      <div style={{ position: 'relative' }}>
+        <Form.Control
+          value={inputValue}
+          onChange={handleInputChange}
+          type="text"
+          disabled={isReadonly}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+};
